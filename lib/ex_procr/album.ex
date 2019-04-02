f7d4225b626f2d0498bac9aa36eb1b7b369a1968
Tyrn <orlov@bos-ltd.ru>
2019-04-02 08:26:25 +0300

    Ready for regex research

--- lib/ex_procr/album.ex
+++ lib/ex_procr/album.ex
@@ -7,9 +7,18 @@ defmodule ExProcr.Album do
   end
 
   @doc """
+  Returns a vector of integer numbers
+  embedded in a string argument.
+  """
+  def str_strip_numbers(s) do
+    s
+  end
+
+  @doc """
   Reduces authors to initials.
   """
-  def make_initials(authors, sep \\ ".", trail \\ ".", hyph \\ "-") do
-    authors
+  def make_initials(authors, _sep \\ ".", _trail \\ ".", _hyph \\ "-") do
+    by_space = fn s -> s end
+    by_space.(authors)
   end
 end
--- test/ex_procr_test.exs
+++ test/ex_procr_test.exs
@@ -8,6 +8,10 @@ defmodule ExProcrTest do
     assert ExProcr.hello() == :world
   end
 
+  test "strips numbers from a string" do
+    assert str_strip_numbers("ab11cdd2k.144") == "ab11cdd2k.144"
+  end
+
   test "makes initials" do
     assert make_initials("bravo") == "bravo"
   end
